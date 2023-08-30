f47a314e5b02e8ec0ab43f6aefb2aa1a565ec812
AndrewKizito <AndrewKizito54@gmail.com>
2023-08-30 09:09:33 +0300

    Notifications Config Added

---
+++ src/utils/reusables.ts
@@ -0,0 +1,10 @@
+import { iNotification } from "react-notifications-component";
+
+const notificationConfig: iNotification = {
+  insert: "top",
+  container: "top-right",
+  animationIn: ["animate__animated animate__fadeIn"],
+  animationOut: ["animate__animated animate__fadeOut"],
+};
+
+export { notificationConfig };
