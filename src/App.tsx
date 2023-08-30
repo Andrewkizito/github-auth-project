95e149d3f9ad3061c009aec00c441f33ca774f0b
AndrewKizito <AndrewKizito54@gmail.com>
2023-08-30 09:06:50 +0300

    Added Auth Wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+// Router components
+import { Routes, Route } from "react-router-dom";
+
+// App Pages
+import Auth from "./pages/Auth";
+import Login from "./pages/Login";
+import Dashboard from "./pages/dashboard/Dashboard";
+
+// UI Components
+import { AuthProvider } from "./components/AuthProvider";
+import { ReactNotifications } from "react-notifications-component";
+
 function App() {
-  return <></>;
+  return (
+    <AuthProvider>
+      <ReactNotifications />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/callback" element={<Auth />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </AuthProvider>
+  );
 }
 
 export default App;
