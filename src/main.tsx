4e123bda269e5beb5fe31f466017096130068b66
AndrewKizito <AndrewKizito54@gmail.com>
2023-08-30 09:07:07 +0300

    Added Core App Setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// App Root
+import App from "./App";
+
+// Router
+import { BrowserRouter } from "react-router-dom";
+
+// Styles
+import "./index.css";
+import "animate.css/animate.min.css";
+import "react-notifications-component/dist/theme.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
