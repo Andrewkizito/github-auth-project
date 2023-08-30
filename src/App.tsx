dc8d30e03ba7b29bddc73f44f10ffec7bb1f030e
AndrewKizito <AndrewKizito54@gmail.com>
2023-08-30 15:55:25 +0300

    Rendering App Root

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-// Router components
-import { Routes, Route } from "react-router-dom";
-
-// App Pages
-import Auth from "./pages/Auth";
-import Login from "./pages/Login";
-import Dashboard from "./pages/dashboard/Dashboard";
-
 // UI Components
 import { AuthProvider } from "./components/AuthProvider";
 import { ReactNotifications } from "react-notifications-component";
+import AppRoot from "./AppRoot";
 
 function App() {
+ 
+
   return (
     <AuthProvider>
       <ReactNotifications />
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/callback" element={<Auth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <AppRoot />
     </AuthProvider>
   );
 }
